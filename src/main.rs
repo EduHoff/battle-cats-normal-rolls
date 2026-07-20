c7e619fe9a92153e83171b82565261221f11cac3
EduHoff <ehcs.business@gmail.com>
2026-07-20 19:25:26 -0300

    feat: local server is running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Not yet ready...");
+use axum::{Router, http::StatusCode, response::Html, routing::get};
+use std::error::Error;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new()
+        .route("/", get(home_page))
+        .nest_service("/static", ServeDir::new("static"));
+
+    let addr = "127.0.0.1:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    println!("Server running on http://{}", addr);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn home_page() -> Result<Html<String>, StatusCode> {
+    tokio::fs::read_to_string("templates/index.html")
+        .await
+        .map(Html)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
